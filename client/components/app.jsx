c802d7235ae215bee13a3fb0863bc25926fcb976
MattWotus <mawotus@gmail.com>
2020-09-22 19:18:41 +0000

    In progress add-visit-front-end

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,22 +1,29 @@
 import React from 'react';
 // import Welcome from './welcome';
-import Visits from './visits';
+// import Visits from './visits';
+import VisitForm from './visitForm';
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      message: null,
-      isLoading: true
+      visits: []
     };
+    this.addVisit = this.addVisit.bind(this);
   }
 
   componentDidMount() {
   }
 
+  addVisit(newVisit) {
+    const newArray = this.state.visits.slice(0, this.state.visits.length);
+    newArray.push(newVisit);
+    this.setState({ visits: newArray });
+  }
+
   render() {
     return (
-      <Visits />
+      <VisitForm onSubmit={this.addVisit} />
     );
   }
 }
