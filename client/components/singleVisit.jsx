6c3dbb32246ec15efe1c1a823a63336e45ef0d08
MattWotus <mawotus@gmail.com>
2020-09-22 23:24:42 +0000

    Completed view-single-visit-front-end except for date and location

--- client/components/singleVisit.jsx
+++ client/components/singleVisit.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import Navbar from './navbar';
 import SingleVisitList from './singleVisitList';
 
-function SingleVisit() {
+function SingleVisit(props) {
   return (
-    <div className="container-fluid">
+    <div className="container-fluid mb-5">
       <div className='row'>
         <div className='col-12 d-flex justify-content-between align-items-center mt-4'>
-          <div className="blueColor">&lt; Back</div>
+          <div className="blueColor backFontSize">&lt; Back</div>
           <div><i className="fas fa-ellipsis-h fa-3x"></i></div>
         </div>
       </div>
--- client/components/singleVisitListItem.jsx
+++ client/components/singleVisitListItem.jsx
@@ -3,9 +3,15 @@ import React from 'react';
 function SingleVisitListItem(props) {
   const test = props.test;
   const testName = test[0];
-  const first = testName[0].charAt(0);
-  const upper = first.toUpperCase();
-  const testNameUpper = upper + testName[0].slice(1);
+  let testNameUpper = '';
+  if (testName[0].length <= 3) {
+    testNameUpper = testName[0].toUpperCase();
+  } else {
+    const first = testName[0].charAt(0);
+    const upper = first.toUpperCase();
+    testNameUpper = upper + testName[0].slice(1);
+  }
+
   const result = props.result;
   return (
     <div className='visit d-flex justify-content-between align-items-center mt-4 mb-4'>
