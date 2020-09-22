cea3c1aecdfc1bf2dd603eaf44a5d876c5bd8cc7
MattWotus <mawotus@gmail.com>
2020-09-22 20:18:19 +0000

    Completed add-visit-front-end feature

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -6,19 +6,11 @@ import VisitForm from './visitForm';
 export default class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      visits: []
-    };
     this.addVisit = this.addVisit.bind(this);
   }
 
-  componentDidMount() {
-  }
-
   addVisit(newVisit) {
-    const newArray = this.state.visits.slice(0, this.state.visits.length);
-    newArray.push(newVisit);
-    this.setState({ visits: newArray });
+
   }
 
   render() {
