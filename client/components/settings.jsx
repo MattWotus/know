5e5354655d1d595331946a8af56788e712f3f999
MattWotus <mawotus@gmail.com>
2020-09-23 22:09:08 +0000

    Completed settings page

--- client/components/settings.jsx
+++ client/components/settings.jsx
@@ -1,26 +1,52 @@
 import React from 'react';
-// import { ProgressPlugin } from 'webpack';
 import Navbar from './navbar';
 
 function Settings(props) {
   return (
-    <div className="visit d-flex justify-content-between align-items-center mt-4 mb-4">
-      <div className="col-11">
-        <div>Email</div>
-        <div>
-          <div className="blueColor">&gt;</div>
+    <div className="container-fluid">
+      <div className="row d-flex justify-content-center">
+        <div className="col-11 d-flex justify-content-center">
+          <h2 className="mt-3 mb-3">Settings</h2>
         </div>
       </div>
-      <div className="col-11">
-        <div>Password</div>
-        <div>
-          <div className="blueColor">&gt;</div>
+      <div className="row d-flex justify-content-center">
+        <div className="col-11 d-flex justify-content-between align-items-center mt-3 mb-3 settings">
+          <div>Email</div>
+          <div>
+            <div className="blueColor">&gt;</div>
+          </div>
         </div>
       </div>
-      <div className="col-11">
-        <div>Log Out</div>
-        <div>
-          <div className="blueColor">&gt;</div>
+      <div className="row d-flex justify-content-center">
+        <div className="col-11 d-flex justify-content-between align-items-center mt-3 mb-3 settings">
+          <div>Password</div>
+          <div>
+            <div className="blueColor">&gt;</div>
+          </div>
+        </div>
+      </div>
+      <div className="row d-flex justify-content-center">
+        <div className="col-11 d-flex justify-content-between align-items-center mt-3 mb-3 settings">
+          <div>Privacy Policy</div>
+          <div>
+            <div className="blueColor">&gt;</div>
+          </div>
+        </div>
+      </div>
+      <div className="row d-flex justify-content-center">
+        <div className="col-11 d-flex justify-content-between align-items-center mt-3 mb-3 settings">
+          <div>Help</div>
+          <div>
+            <div className="blueColor">&gt;</div>
+          </div>
+        </div>
+      </div>
+      <div className="row d-flex justify-content-center">
+        <div onClick={() => props.setView('welcome')}className="col-11 d-flex justify-content-between align-items-center mt-3 mb-3 settings">
+          <div>Log Out</div>
+          <div>
+            <div className="blueColor">&gt;</div>
+          </div>
         </div>
       </div>
       <div className='row'>
