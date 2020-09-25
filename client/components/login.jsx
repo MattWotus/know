17686bd3371c839f3447fe6b694a96efd81e52f6
kimballtyler <kimballstyler@gmail.com>
2020-09-25 15:24:05 -0400

    working on login front

--- client/components/login.jsx
+++ client/components/login.jsx
@@ -7,10 +7,50 @@ class Login extends React.Component {
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleReset = this.handleReset.bind(this);
+    this.validateFields = this.validateFields.bind(this);
+    this.validateCredentials = this.validateCredentials.bind(this);
     this.state = {
       email: '',
-      password: ''
+      password: '',
+      errorMessage: ''
     };
+  }
+
+  validateFields() {
+    let errorMessage = '';
+    if (!this.state.email || !this.state.password) {
+      errorMessage = 'email and password are required fields';
+    } else if (!this.state.email.includes('@')) {
+      errorMessage = 'invalid email';
+    }
+    if (errorMessage) {
+      this.setState({ errorMessage });
+      return false;
+    }
+    this.setState({ errorMessage: '' });
+    return true;
+  }
+
+  validateCredentials() {
+    const user = {
+      email: this.state.email,
+      password: this.state.password
+    };
+    let result;
+    fetch('/api/signin', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(user)
+    })
+      .then(result => {
+        if (result.ok === false) {
+          this.setState(() => ({ errorMessage: 'username and password do not match' }));
+        }
+        result = true;
+      });
+    return result;
   }
 
   handleChange(event) {
@@ -21,19 +61,14 @@ class Login extends React.Component {
 
   handleSubmit(event) {
     event.preventDefault();
-    const user = {
-      email: this.state.email,
-      password: this.state.password
-    };
-    fetch('/api/signin', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(user)
-    });
-    this.handleReset();
-    this.props.setView('visits');
+    const isValid = this.validateFields();
+    if (isValid) {
+      const credentials = this.validateCredentials();
+      if (credentials) {
+        this.props.setView('visits');
+        this.handleReset();
+      }
+    }
   }
 
   handleReset() {
@@ -56,12 +91,15 @@ class Login extends React.Component {
           <div className="col-10">
             <form id="signinForm" onSubmit={this.handleSubmit} onReset={this.handleReset}>
               <div className="form-group">
-                <input type="email" className="form-control" id="email" placeholder="Email" />
+                <input onChange={this.handleChange} name="email" type="email" className="form-control" id="email" placeholder="Email" required />
               </div>
               <div className="form-group">
-                <input type="password" className="form-control" id="password" placeholder="Password" />
+                <input onChange={this.handleChange} name="password" type="password" className="form-control" id="password" placeholder="Password" required />
+                {this.state.errorMessage ? <div className="alert alert-danger mt-2" role="alert">
+                  {this.state.errorMessage}
+                </div> : null}
               </div>
-              <button onClick={() => this.props.setView('visits')} type="submit" id="login" className="btn mt-3"><h5 className="mb-0">Login</h5></button>
+              <button type="submit" id="login" className="btn mt-2"><h5 className="mb-0">Login</h5></button>
               <small onClick={() => this.props.setView('signup')} id="createAccount" className="cursor-pointer form-text text-muted mt-4"><p>Create Account</p></small>
             </form>
           </div>
